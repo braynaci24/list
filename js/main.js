fad586e1d79476bbe0c7a4b7c0c95a0f7425f49e
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-22 17:36:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    let generalInfo = []
+    $('.info-button').click(function () {
+
+        let inputVal = $('.todo-input').val();
+
+        generalInfo.push(inputVal);
+
+        if ($('.info').length < 3 && inputVal.length >= 2) {
+
+            $('.append-information-container').append(`<th class="info"> ${inputVal}</th>`);
+            $('.todo-input').val("")
+            if ($('.info').length == 3) {
+                $('.todo-input-container').remove();
+                $('.information-button').show();
+            }
+        }
+    })
+    $('.information-button').click(function () {
+        $('.table ').remove();
+        $('.info-card').append(`<div class="alert alert-danger card" role="alert">Bilgileriniz isim, soyisim ve yaş olmak üzere şu şekildedir:  <span class="card-text"> ${generalInfo} </span> </div>`)
+        $('.information-button').remove();
+        $('.information-button-2').show();
+    })
+    $('.information-button-2').click(function(){
+        window.location.reload();
+    })
+
+})
